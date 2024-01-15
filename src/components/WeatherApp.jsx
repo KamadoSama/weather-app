fcd302e2053325c6b55f0741fb1d8114ec31bc2b
tahoarthur <tahoarthur@gmail.com>
2024-01-15 10:34:48 +0100

    Refactor WeatherApp component to separate data fetching and rendering logic

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -11,19 +11,8 @@ import humidity_icon from "../assets/humidity.png";
 import drizzle_icon from "../assets/drizzle.png";
 const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 const WeatherApp = () => {
-  
 
-  const [wicon, setWicon] = useState(cloud_icon);
-  const search = async () => {
-    const element = document.getElementsByClassName("cityInput");
-    if (element[0].value === "") {
-      return;
-    }
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${API_KEY}`;
-    let response = await fetch(url);
-
-    let data = await response.json();
-    console.log(data);
+  const setData = (data) => {
     const humidity = document.getElementsByClassName("humidity-percent");
     const wind = document.getElementsByClassName("wind-rate");
     const temperature = document.getElementsByClassName("weather-temp");
@@ -69,7 +58,24 @@ const WeatherApp = () => {
     } else {
       setWicon(clear_icon);
     }
+  }
+
+  const [wicon, setWicon] = useState(cloud_icon);
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${API_KEY}`;
+    let response = await fetch(url);
+
+    let data = await response.json();
+    console.log(data);
+    
+    setData(data);
   };
+
+  
 
   const weatherByLocation = async ({ latitude, longitude }) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=Metric`;
@@ -77,15 +83,9 @@ const WeatherApp = () => {
 
     let data = await response.json();
     console.log(data);
-    const humidity = document.getElementsByClassName("humidity-percent");
-    const wind = document.getElementsByClassName("wind-rate");
-    const temperature = document.getElementsByClassName("weather-temp");
-    const location = document.getElementsByClassName("weather-location");
 
-    humidity[0].innerText = data.main.humidity + " %";
-    wind[0].innerText = data.wind.speed + " km/h";
-    temperature[0].innerText = data.main.temp + " °C";
-    location[0].innerText = data.name;
+    setData(data);
+    
   };
 
   useEffect(() => {
@@ -97,6 +97,7 @@ const WeatherApp = () => {
       weatherByLocation({ latitude, longitude });
     });
   }, []);
+
   return (
     <>
       <div className="container">
